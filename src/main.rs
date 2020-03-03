f957fed51ec4616f513207bd71219ea7fdcb78b3
scturtle <scturtle@gmail.com>
2020-03-03 08:49:57 +0800

    WIP election

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,207 @@
+#![allow(dead_code)]
+
+use async_std::sync::{channel, Receiver, Sender};
+use async_std::{stream, stream::StreamExt};
+use futures::{future::FutureExt, join, select};
 use log::info;
-use std::{
-    collections::HashMap,
-    future::Future,
-    pin::Pin,
-    sync::mpsc::{channel, Receiver, Sender},
-    sync::{Arc, Mutex},
-    task::{Context, Poll, Waker},
-    thread,
-};
+use rand::{thread_rng, Rng};
+use std::collections::BTreeMap;
+use std::time::{Duration, Instant};
 
-pub type ServerId = u32;
-type MsgId = u32;
-type FutKey = (ServerId, MsgId);
+struct LogEntry {
+    cmd: String,
+    term: i32,
+}
+
+#[derive(Clone)]
+struct RequestVoteArgs {
+    term: i32,
+    candidate_id: i32,
+    // last_log_index: i32,
+    // last_log_term: i32,
+}
+
+struct RequestVoteReply {
+    term: i32,
+    vote_granted: bool,
+}
+
+struct AppendEntriesArgs {
+    term: i32,
+    leader_id: i32,
+    // prev_log_index: i32,
+    // prev_log_term: i32,
+    // entries: Vec<LogEntry>,
+    // leader_commit: i32,
+}
+
+struct AppendEntriesReply {
+    term: i32,
+    success: bool,
+}
+
+enum Msg {
+    RequestVoteArgs(RequestVoteArgs),
+    RequestVoteReply(RequestVoteReply),
+    AppendEntriesArgs(AppendEntriesArgs),
+    AppendEntriesReply(AppendEntriesReply),
+}
+
 #[derive(PartialEq)]
-enum MsgType { REQ, REP }
-struct Message(FutKey, MsgType, String); // true for request, false for reply
-
-struct ServerState {
-    peer_senders: HashMap<ServerId, Sender<Message>>,
-    fut_states: HashMap<FutKey, Arc<Mutex<ReplyState>>>,
+enum NodeState {
+    Follower,
+    Candidate,
+    Leader,
 }
 
-pub struct RPCServer {
-    pub id: ServerId,
-    sender: Sender<Message>,
-    state: Arc<Mutex<ServerState>>,
+struct LeaderState {
+    next_index: BTreeMap<i32, i32>,
+    match_index: BTreeMap<i32, i32>,
 }
 
-impl RPCServer {
-    pub fn new(id: ServerId) -> Self {
-        let (sender, receiver) = channel();
-        let state = Arc::new(Mutex::new(ServerState {
-            peer_senders: HashMap::new(),
-            fut_states: HashMap::new(),
-        }));
+struct ElectionState {
+    term: i32,
+    votes: i32,
+}
 
-        let thread_state = state.clone();
-        thread::spawn(move || RPCServer::run(id, receiver, thread_state));
+struct Node {
+    id: i32,
+    sender: Sender<Msg>,
+    receiver: Receiver<Msg>,
+    peers: BTreeMap<i32, Sender<Msg>>,
+    // persistent
+    curr_term: i32,
+    vote_for: Option<i32>,
+    logs: Vec<LogEntry>,
+    // all state
+    commit_index: i32,
+    last_applied: i32,
+    state: NodeState,
+    election_reset_event: Instant,
+    // leader state
+    leader_state: Option<LeaderState>,
+    // tmp state
+    election_state: Option<ElectionState>,
+}
 
-        RPCServer { id, sender, state }
+impl Node {
+    pub fn new(id: i32) -> Node {
+        let (sender, receiver) = channel(100);
+        Node {
+            id: id,
+            sender: sender,
+            receiver: receiver,
+            peers: BTreeMap::new(),
+            curr_term: 0,
+            vote_for: None,
+            logs: Vec::new(),
+            commit_index: -1,
+            last_applied: -1,
+            state: NodeState::Follower,
+            election_reset_event: Instant::now(),
+            leader_state: None,
+            election_state: None,
+        }
     }
 
-    fn run(id: ServerId, receiver: Receiver<Message>, state: Arc<Mutex<ServerState>>) {
+    pub fn add_peer(&mut self, node: &Node) {
+        assert_ne!(self.id, node.id);
+        self.peers.insert(node.id, node.sender.clone());
+    }
+
+    pub async fn run(&mut self) {
+        let mut interval = stream::interval(Duration::from_millis(10));
         loop {
-            if let Ok(Message(fut_key, typ, msg)) = receiver.recv() {
-                let mut state = state.lock().unwrap();
-                match typ {
-                    MsgType::REQ => {
-                        let peer_sender = state
-                            .peer_senders
-                            .get(&fut_key.0)
-                            .expect("peer sender not found")
-                            .clone();
-                        peer_sender
-                            .send(Message((id, fut_key.1), MsgType::REP, msg))
-                            .expect("send error");
-                    },
-                    MsgType::REP => {
-                        let fut_state = state.fut_states.remove(&fut_key).expect("fut not found");
-                        fut_state.lock().unwrap().wake(msg);
+            select! {
+                _ = interval.next().fuse() => {
+                    info!("{} tick", self.id);
+                },
+                msg = self.receiver.recv().fuse() => {
+                    info!("{} recv", self.id);
+                    match msg {
+                        Some(Msg::RequestVoteArgs{..}) => {
+                            // TODO
+                        },
+                        Some(Msg::RequestVoteReply(reply)) => {
+                            self.process_vote_reply(reply);
+                        },
+                        Some(Msg::AppendEntriesArgs{..}) => {
+                            // TODO
+                        },
+                        Some(Msg::AppendEntriesReply{..}) => {
+                            // TODO
+                        }
+                        _ => {}
                     }
                 }
-            } else {
-                break;
             }
         }
     }
 
-    pub fn add_peer(&self, peer: &RPCServer) {
-        let mut state = self.state.lock().unwrap();
-        state.peer_senders.insert(peer.id, peer.sender.clone());
+    fn election_timeout(&self) -> Duration {
+        Duration::from_millis(thread_rng().gen_range(150, 300))
     }
 
-    pub async fn send_request(&self, peer_id: ServerId, msg_id: MsgId, msg: String) -> String {
-        let mut state = self.state.lock().unwrap();
-        let peer_sender = state
-            .peer_senders
-            .get(&peer_id)
-            .expect("peer sender not found");
-        peer_sender
-            .send(Message((self.id, msg_id), MsgType::REQ, msg))
-            .expect("send error");
-        let fut_key = (peer_id, msg_id);
-        assert!(!state.fut_states.contains_key(&fut_key), "msg sent already");
-        let fut = ReplyFuture::new();
-        state.fut_states.insert(fut_key, fut.get_state());
-        drop(state); // unlock
-        fut.await
-    }
-}
-
-struct ReplyState {
-    reply: Option<String>,
-    waker: Option<Waker>,
-}
-
-impl ReplyState {
-    fn wake(&mut self, msg: String) {
-        self.reply = Some(msg);
-        if let Some(waker) = self.waker.take() {
-            waker.wake();
+    async fn start_eletion(&mut self) {
+        self.state = NodeState::Candidate;
+        self.curr_term += 1;
+        self.election_reset_event = Instant::now();
+        self.vote_for = Some(self.id);
+        self.election_state = Some(ElectionState {
+            term: self.curr_term,
+            votes: 1,
+        });
+        info!(
+            "{} become candidate and start eletion at term {}",
+            self.id, self.curr_term
+        );
+        // request vote to all other servers
+        let msg = RequestVoteArgs {
+            term: self.curr_term,
+            candidate_id: self.id,
+        };
+        for (_peer_id, peer_sender) in self.peers.iter() {
+            peer_sender.send(Msg::RequestVoteArgs(msg.clone())).await;
         }
     }
-}
 
-struct ReplyFuture {
-    state: Arc<Mutex<ReplyState>>,
-}
-
-impl ReplyFuture {
-    fn new() -> Self {
-        let state = Arc::new(Mutex::new(ReplyState {
-            reply: None,
-            waker: None,
-        }));
-        ReplyFuture { state }
-    }
-    fn get_state(&self) -> Arc<Mutex<ReplyState>> {
-        self.state.clone()
-    }
-}
-
-impl Future for ReplyFuture {
-    type Output = String;
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut state = self.state.lock().unwrap();
-        if let Some(reply) = state.reply.take() {
-            Poll::Ready(reply)
-        } else {
-            state.waker = Some(cx.waker().clone());
-            Poll::Pending
+    fn process_vote_reply(&mut self, reply: RequestVoteReply) {
+        if self.state != NodeState::Candidate {
+            return;
         }
+        let election_state = self.election_state.as_mut().unwrap();
+        if reply.term > election_state.term {
+            info!("{} update term {}", self.id, reply.term);
+            // TODO become follower
+            return;
+        } else if reply.term == election_state.term && reply.vote_granted {
+            election_state.votes += 1;
+            if election_state.votes * 2 > self.peers.len() as i32 + 1 {
+                // TODO start leader
+            }
+        }
+    }
+
+    fn become_follower(&mut self, term: i32) {
+        info!("{} become follower with term {}", self.id, term);
+        self.state = NodeState::Follower;
+        self.curr_term = term;
+        self.election_reset_event = Instant::now();
+        self.election_state = None;
     }
 }
 
 async fn test() {
-    let sv1 = RPCServer::new(1);
-    let sv2 = RPCServer::new(2);
-    sv1.add_peer(&sv2);
-    sv2.add_peer(&sv1);
-    let req = "hello".to_string();
-    info!("req: {}", &req);
-    let rep = sv1.send_request(sv2.id, 0, req).await;
-    info!("rep: {}", rep);
+    let mut node1 = Node::new(1);
+    let mut node2 = Node::new(2);
+    let mut node3 = Node::new(3);
+    node1.add_peer(&node2);
+    node1.add_peer(&node3);
+    node2.add_peer(&node1);
+    node2.add_peer(&node3);
+    node3.add_peer(&node1);
+    node3.add_peer(&node2);
+    join!(node1.run(), node2.run(), node3.run());
 }
 
 fn main() {
